806eda2fbb81408041b991abe6e01a03dcfbe7c7
ahmettalhatelli <ahmettalhatelli@192.168.1.36>
2022-04-29 19:58:16 +0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+  const [city,setcity]=useState() ;
+  const key="dbe08e166bab67e386089961e0967471";
+  useEffect(() => { 
+    async function getApi() {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${key}&units=metric`);
+      console.log(response);
+      setcity=(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+    getApi();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+       <input className='border-8' type="text"/>
+     </div>
     </div>
   );
-}
-
-export default App;
+  
+} export default App;
