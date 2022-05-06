301acbc245cfedcb5376b42a77a4f388db075af4
talhatelli <ahmettalhatelli1453@gmail.com>
2022-05-06 13:49:24 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
-
+import City from './City';
 
 function App() {
-  const [city,setcity]=useState() ;
   const key="dbe08e166bab67e386089961e0967471";
+  const[search,setSearch]=useState("");
+  const [city,setCity]=useState() ;
+ 
   useEffect(() => { 
     async function getApi() {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${key}&units=metric`);
+      const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+      );
       console.log(response);
-      setcity=(response.data);
+      setCity(response.data);
     } catch (error) {
       console.error(error);
     }
   }
     getApi();
-  },[]);
+  },[search]);
+  console.error(search);
 
   return (
     <div className="App">
-     <div>
-       <input className='border-8' type="text"/>
-     </div>
+    <div>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Placeholder"
+        className=" my-5 px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600   bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring "
+      />
+      {city && <City city={city} />}
     </div>
+  </div>
   );
-  
 } export default App;
